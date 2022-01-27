1f14f2612a6f7e3adc04474e7b4516a175770980
Justin Luciano <justinlluciano@gmail.com>
2022-01-27 10:29:16 -0800

    saving

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+// const isWin = (season) => {
+//   return season.result === 'W';
+// }
 
+// function superbowlWin(record) {
+//   const season = record.find(isWin);
+//   if (season != undefined) {
+//     return season.year;
+//   } else {
+//     return season;
+//   }
+// }
+
+const superbowlWin = (record) => {
+  const season = record.find((season) => season.result === 'W');
+  if (season != undefined) {
+    return season.year;
+  } else {
+    return season;
+  }
+};
